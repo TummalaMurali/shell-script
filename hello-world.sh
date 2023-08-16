579a9d643832b5188d74825f01fded0b409efb1f
TummalaMurali <mctummala@yahoo.com>
2023-08-16 16:05:38 -0700

    values passed with variables

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Hello world, I am learning DevOps with AWS"
+
+echo "Hello World, I am learning DevOps with AWS"
+PERSON1=Sachin
+PERSON2=Rohit
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
