8fbfc2f1af367bd174807d0d92e22c4393d7d3f6
TummalaMurali <mctummala@yahoo.com>
2023-08-17 13:46:33 -0700

    writing logs into file

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-# our program goal is to install mysql & postfix
+# our program goal is to install mysql
+
+DATE=$(date +%F)
+SCRIPT_NAME=$0
+LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 # this function should validate the previous command and inform user it is success or failure
 VALIDATE(){
     #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]
     then
-        echo "Installation ... FAILURE"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo "Installtion ... SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
+
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
@@ -24,10 +33,10 @@ then
 fi
 
 # it is our responsibility again to check installation is success or not
-yum install mysql -y
+yum install mysql -y &>>$LOGFILE
 
-VALIDATE $?
+VALIDATE $? "Installing MySQL"
 
-yum install postfix -y
+yum install posddtfix -y &>>$LOGFILE
 
-VALIDATE $?
+VALIDATE $? "Installing postfix"
